f7c70668d7a23fc244be3a31150fe2452c25ffdc
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-01-07 14:49:59 +0900

    [level 2] Title: 기능개발, Time: 2.97 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,26 +3,26 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] days = new int[progresses.length];
+        
         for(int i=0; i<days.length; i++){
             int temp = 100 - progresses[i];
             days[i] = temp%speeds[i]==0 ? temp/speeds[i] : temp/speeds[i] + 1;
         }
 
-        Queue<Integer> queue = new LinkedList<>();
-        int count = 1;
+        Queue<Integer> q = new LinkedList<>();
+        int count = 1, prev = days[0];
         
-        int prev = days[0];
         for(int i=1; i<days.length; i++){
             if (days[i] <= prev)
                 count++;
-            else{
-                queue.add(count);
+            else {
+                q.add(count);
                 count = 1;
                 prev = days[i];
             }
         }
-        queue.add(count);
+        q.add(count);
 
-        return queue.stream().mapToInt(i->i).toArray();
+        return q.stream().mapToInt(i->i).toArray();
     }
 }
