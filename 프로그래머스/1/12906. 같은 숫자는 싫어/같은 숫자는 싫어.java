f1e23196ee28543c8190ce5e93310fce4ec6bc70
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-01-07 12:25:36 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 47.27 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<arr.length; i++){
+            if (i==0 || stack.peek() != arr[i])
+                stack.push(arr[i]);
+        }
+
+        return stack.stream().mapToInt(i -> i).toArray();
+    }
+}
