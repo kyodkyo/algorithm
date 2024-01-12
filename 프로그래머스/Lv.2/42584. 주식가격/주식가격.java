5a32d4e894e086fb03cb78405886d4ef00203915
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-01-12 15:45:16 +0900

    [level Lv.2] Title: 주식가격, Time: 20.41 ms, Memory: 68.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/42584. 주식가격/주식가격.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        Queue<Integer> q = new LinkedList<>();
+        int cur = 0;
+        
+        while(true){
+            int count = 0;
+            for(int i=cur+1; i<prices.length; i++){
+                count++;
+                
+                if (prices[i] < prices[cur])
+                    break;
+            }
+            q.add(count);
+            cur++;
+            
+            if (cur == prices.length)
+                break;
+        }
+        
+        return q.stream().mapToInt(i->i).toArray();
+    }
+}
