6cbffc591cfba191f054750e9b5e831c81cfed3a
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-01-26 20:51:33 +0900

    [level 1] Title: 체육복, Time: 0.54 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n - lost.length;
+        
+        Queue<Integer> q = new LinkedList<>();
+        Arrays.sort(lost);
+        
+        for(int num : reserve)
+            q.add(num);
+        
+        for(int i=0; i<lost.length; i++){
+            if (q.contains(lost[i])){
+                q.remove(lost[i]);
+                lost[i] = -1;
+                answer++;
+            }
+        }
+        
+        for(int i=0; i<lost.length; i++){
+            if (q.contains(lost[i]-1) || q.contains(lost[i]+1)){
+                if (q.contains(lost[i]-1))
+                    q.remove(lost[i]-1);
+                else if (q.contains(lost[i]+1))
+                    q.remove(lost[i]+1);
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
