cadc7364525f60df1c676eaba6ae809eb679395f
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-03-02 17:12:25 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 628 ms, Memory: 65520 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
@@ -1,32 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main{
-    static class Level{
-        String name;
-        int value;
-
-        public Level(String name, int value){
-            this.name = name;
-            this.value = value;
-        }
-    }
-    static List<Level> list = new ArrayList<>();
+    static int n, m;
+    static int[] values;
+    static String[] names;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
+
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+        values = new int[n];
+        names = new String[n];
 
         for(int i=0; i<n; i++){
             String[] line = br.readLine().split(" ");
-            list.add(new Level(line[0], Integer.parseInt(line[1])));
+            names[i] = line[0];
+            values[i] = Integer.parseInt(line[1]);
         }
 
         for(int i=0; i<m; i++)
@@ -36,16 +30,16 @@ public class Main{
 
     public static void searchLevel(int cur){
         int left = 0;
-        int right = list.size()-1;
+        int right = n-1;
 
         while (left <= right){
             int mid = (left + right) / 2;
 
-            if (list.get(mid).value < cur)
+            if (values[mid] < cur)
                 left = mid + 1;
             else
                 right = mid - 1;
         }
-        sb.append(list.get(left).name + "\n");
+        sb.append(names[left] + "\n");
     }
 }
