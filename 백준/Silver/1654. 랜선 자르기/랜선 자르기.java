d11a74c93b951a36119f3fba1a3ba6851eff5ccb
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-03-14 01:02:39 +0900

    [Silver II] Title: 랜선 자르기, Time: 160 ms, Memory: 17616 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,44 +1,73 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
-public class Main{
-    static int K, N;
-    static long max=0;
-    static int[] arr;
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        K = Integer.parseInt(input[0]);
-        N = Integer.parseInt(input[1]);
+/*
+ * 모든 매개변수를 완전탐색 하지말 이분탐색으로 줄이자.
+ */
+public class Main {
+    public static int N, K;
+    public static int[] arr;
+    public static void main(String[] args) throws Exception {
+        K = read(); // 랜선의 수
+        N = read(); // 랜선으 ㅣ길이
+        long ans = 0;
 
         arr = new int[K];
-        for(int i=0; i<K; i++){
-            arr[i] = Integer.parseInt(br.readLine());
-            max = Math.max(max, arr[i]);
+
+        for(int i = 0; i < K; i++){
+            arr[i] =  read();
         }
 
-        System.out.println(binary( ));
+        Arrays.sort(arr);
+
+        System.out.println(binarySearch());
     }
-    
-    public static long binary(){
-        long low = 0;
-        long high = max + 1;
-        
-        while (low < high){
-            long mid = (low + high) / 2;
-            
-            long count = 0;
-            for(int len : arr)
-                count += (len / mid);
-            
-            if (count >= N)
-                low = mid + 1;
-            else 
-                high = mid;
+
+    public static long binarySearch(){
+        long s = 1;
+        long e = Integer.MAX_VALUE;
+        long ans = 0;
+
+        while(s<=e) {
+            long mid = (s + e) / 2;
+            long now = 0;
+            for(int i = 0; i < K; i++){
+                now += arr[i]/mid;
+            }
+            if(now >= N){
+                s = mid + 1;
+                ans = mid;
+            }
+            else{
+                e = mid -1;
+            }
         }
-        
-        return low - 1;
+        return ans;
     }
+
+
+
+
+    public static int read() throws Exception{
+        int n = 0;
+        int cur;
+        boolean isNumber = false;
+        boolean isNegative = false;
+        while(true){
+            cur = System.in.read();
+            if(cur == '-'){
+                isNegative = true;
+            }
+            else if(cur <= 32){
+                if(isNumber){
+                    return isNegative ? -n : n;
+                }
+            }
+            else{
+                isNumber = true;
+                n = (n<<3) + (n<<1) + (cur&15);
+            }
+        }
+    }
+
 }
