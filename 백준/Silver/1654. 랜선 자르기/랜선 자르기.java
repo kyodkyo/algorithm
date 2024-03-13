3fb57b1d589ca09c073ce738770afb681b1ff045
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-03-14 00:26:39 +0900

    [Silver II] Title: 랜선 자르기, Time: 176 ms, Memory: 16776 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -3,34 +3,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main{
+    static int K, N;
+    static long max=0;
+    static int[] arr;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
-        int K = Integer.parseInt(input[0]);
-        int N = Integer.parseInt(input[1]);
+        K = Integer.parseInt(input[0]);
+        N = Integer.parseInt(input[1]);
 
-        int[] arr = new int[K];
-        long max = 0;
+        arr = new int[K];
         for(int i=0; i<K; i++){
             arr[i] = Integer.parseInt(br.readLine());
             max = Math.max(max, arr[i]);
         }
 
-        max += 1;
-        long min = 0;
-
-        while (min < max){
-            long mid = (min + max) / 2;
-
+        System.out.println(binary( ));
+    }
+    
+    public static long binary(){
+        long low = 0;
+        long high = max + 1;
+        
+        while (low < high){
+            long mid = (low + high) / 2;
+            
             long count = 0;
             for(int len : arr)
                 count += (len / mid);
             
-            if (count < N)
-                max = mid;
-            else
-                min = mid + 1;
+            if (count >= N)
+                low = mid + 1;
+            else 
+                high = mid;
         }
-        System.out.println(min - 1);
+        
+        return low - 1;
     }
 }
