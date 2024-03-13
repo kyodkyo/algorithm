e2c9d88b48b69dfe017cf709655264eba08cd4bd
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-03-14 01:27:21 +0900

    Update 랜선 자르기.java

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,73 +1,44 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-/*
- * 모든 매개변수를 완전탐색 하지말 이분탐색으로 줄이자.
- */
-public class Main {
-    public static int N, K;
-    public static int[] arr;
-    public static void main(String[] args) throws Exception {
-        K = read(); // 랜선의 수
-        N = read(); // 랜선으 ㅣ길이
-        long ans = 0;
+public class Main{
+    static int K, N;
+    static long low, high;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        K = Integer.parseInt(input[0]);
+        N = Integer.parseInt(input[1]);
 
         arr = new int[K];
-
-        for(int i = 0; i < K; i++){
-            arr[i] =  read();
+        low = 1;
+        high = 0;
+        for(int i=0; i<K; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            high = Math.max(high, arr[i]);
         }
 
-        Arrays.sort(arr);
-
-        System.out.println(binarySearch());
+        System.out.println(binary());
     }
 
-    public static long binarySearch(){
-        long s = 1;
-        long e = Integer.MAX_VALUE;
-        long ans = 0;
+    public static long binary(){
+        long mid = (low + high) / 2;
 
-        while(s<=e) {
-            long mid = (s + e) / 2;
-            long now = 0;
-            for(int i = 0; i < K; i++){
-                now += arr[i]/mid;
-            }
-            if(now >= N){
-                s = mid + 1;
-                ans = mid;
-            }
-            else{
-                e = mid -1;
-            }
+        while (low <= high){
+            long count = 0;
+            for(int len : arr)
+                count += (len / mid);
+
+            if (count >= N)
+                low = mid + 1;
+            else
+                high = mid - 1;
+
+            mid = (low + high) / 2;
         }
-        return ans;
+        return mid;
     }
-
-
-
-
-    public static int read() throws Exception{
-        int n = 0;
-        int cur;
-        boolean isNumber = false;
-        boolean isNegative = false;
-        while(true){
-            cur = System.in.read();
-            if(cur == '-'){
-                isNegative = true;
-            }
-            else if(cur <= 32){
-                if(isNumber){
-                    return isNegative ? -n : n;
-                }
-            }
-            else{
-                isNumber = true;
-                n = (n<<3) + (n<<1) + (cur&15);
-            }
-        }
-    }
-
 }
