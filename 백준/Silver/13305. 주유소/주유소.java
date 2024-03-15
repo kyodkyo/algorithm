5439cc57b500d143ae109c63d4a663b88b5992f3
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-03-16 01:39:26 +0900

    [Silver III] Title: 주유소, Time: 444 ms, Memory: 37300 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -2,34 +2,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main{
-    static int[] distance;
-    static int[] price;
+    static long[] distance;
+    static long[] price;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        distance = new int[n-1];
-        String[] line = br.readLine().split(" ");
-        for(int i=0; i<n-1; i++)
-            distance[i] = Integer.parseInt(line[i]);
+        int j = 0;
+        distance = new long[n-1];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        while(st.hasMoreTokens())
+            distance[j++] = Integer.parseInt(st.nextToken());
 
-        price = new int[n];
-        line = br.readLine().split(" ");
-        for(int i=0; i<n; i++)
-            price[i] = Integer.parseInt(line[i]);
+        j = 0;
+        price = new long[n];
+        st = new StringTokenizer(br.readLine(), " ");
+        while(st.hasMoreTokens())
+            price[j++] = Integer.parseInt(st.nextToken());
 
-        int money = distance[0] * price[0];
-        System.out.println(minPrice(money));
-    }
+        long money = 0;
+        long minPrice = Integer.MAX_VALUE;
 
-    public static int minPrice(int money){
-        int minPrice = price[0];
-        for(int i=1; i< distance.length; i++){
-            minPrice = Math.min(minPrice, price[i]);
-            money += (distance[i] * minPrice);
+        for(int i = 0; i < n-1; i++){
+            if (minPrice > price[i])
+                minPrice = price[i];
+            money += (long) distance[i] * minPrice;
         }
-        return money;
+        System.out.println(money);
     }
+
 }
