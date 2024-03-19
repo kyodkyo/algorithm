35619bf654aa090e880ff8e7291fb17de37b6288
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-03-19 22:53:25 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int[] board = new int[101];
+
+        for(String s : input){
+            int k = Integer.parseInt(s);
+
+            while(k-- > 0){
+                String[] line = br.readLine().split(" ");
+                int start = Integer.parseInt(line[0]);
+                int end = Integer.parseInt(line[1]);
+                board[start] = end;
+            }
+        }
+        bfs(board);
+    }
+
+    public static void bfs(int[] board){
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[101];
+        int[] count = new int[101];
+
+        q.add(1);
+        visited[1] = true;
+
+        while (!q.isEmpty()){
+            int cur = q.poll();
+
+            if (cur == 100) {
+                System.out.println(count[100]);
+                return;
+            }
+
+            for(int i=1; i<=6; i++){
+                int pos = cur + i;
+
+                if (pos > 100 || visited[pos])
+                    continue;
+                visited[pos] = true;
+
+                if (board[pos] != 0){
+                    if (!visited[board[pos]]) {
+                        q.add(board[pos]);
+                        visited[board[pos]] = true;
+                        count[board[pos]] = count[cur] + 1;
+                    }
+                }
+                else{
+                    q.add(pos);
+                    count[pos] = count[cur] + 1;
+                }
+
+            }
+        }
+    }
+}
