5efc5a8afd14e7eaa0a191b7239124acc9efbbcc
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-04-11 16:59:55 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 752 ms, Memory: 67408 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main{
     static int n, m, board[][];
