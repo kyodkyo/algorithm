cb4a90ed433d41085da6ed7675d100f1e01158b1
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-04-17 21:53:42 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 412 ms, Memory: 157716 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        while(t-- > 0){
+            int n = Integer.parseInt(br.readLine());
+            int[][] dp = new int[10001][4];
+            dp[1][1] = 1;
+            dp[2][1] = 1; dp[2][2] = 1;
+            dp[3][1] = 1; dp[3][2] = 1; dp[3][3] = 1;
+
+            for(int i=4; i<=n; i++){
+                dp[i][1] = dp[i-1][1];
+                dp[i][2] = dp[i-2][1] + dp[i-2][2];
+                dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+            }
+            sb.append(dp[n][1] + dp[n][2] + dp[n][3] + "\n");
+        }
+        System.out.println(sb);
+    }
+}
