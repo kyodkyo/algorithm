44c6bc8006984d2e219dc510e15ac2c4f3ed11f3
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-05-01 00:38:31 +0900

    [level 2] Title: 숫자의 표현, Time: 0.46 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n) {
+        int count = 0;
+        
+        int left=1, right=1, sum=0;
+        while (left <= n){
+            if (sum == n){
+                count++;
+                sum -= left;
+                left++;
+            }
+            else if (sum < n){
+                sum += right;
+                right++;
+            }
+            else{
+                sum -= left;
+                left++;
+            }
+        }
+        return count;
+    }
+}
