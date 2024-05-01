853669095c47ae4ce94e2758ab81531c741814fd
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-05-02 01:05:25 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,27 +1,8 @@
 class Solution {
     public int solution(int n) {
-        String binary = Integer.toBinaryString(n);
-        int count = 0;
-        for(int i=0; i<binary.length(); i++){
-            if (binary.charAt(i) == '1')
-                count++;
-        }
-        
-        boolean find = false;
-        while(!find){
-            n += 1;
-            String tmp = Integer.toBinaryString(n);
-            int next = 0;
-            for(int i=0; i<tmp.length(); i++){
-                if (tmp.charAt(i) == '1')
-                    next++;
-            }
-            
-            if (next == count){
-                find = true;
-                break;
-            }
-        }
+        int count = Integer.bitCount(n);
+        while(count != Integer.bitCount(++n))
+            ;
         return n;
     }
 }
