e92d1caa9d8b8b4cdda629c813326d7f78ded233
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-05-02 17:42:38 +0900

    [level 2] Title: 피보나치 수, Time: 3.80 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int[] memo = new int[n+1];
+        memo[0] = 0;
+        memo[1] = 1;
+        
+        for(int i=2; i<=n; i++)
+            memo[i] = (memo[i-1] + memo[i-2]) % 1234567;
+        
+        return memo[n] % 1234567;
+    }
+}
