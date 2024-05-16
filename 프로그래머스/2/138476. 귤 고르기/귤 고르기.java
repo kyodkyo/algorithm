ec5113d738bec063ab953b739e92a7d574311b4a
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-05-16 15:57:39 +0900

    [level 2] Title: 귤 고르기, Time: 63.92 ms, Memory: 93.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for(int i=0; i<tangerine.length; i++)
+            hashMap.put(tangerine[i], hashMap.getOrDefault(tangerine[i], 0) + 1);
+
+        List<Integer> list = new ArrayList<>(hashMap.keySet());
+        list.sort((o1, o2) -> hashMap.get(o2) - hashMap.get(o1));
+
+        int sum = 0;
+        int kind = 0;
+        for(int key : list){
+            kind++;
+            sum += hashMap.get(key);
+
+            if (sum >= k)
+                break;
+        }
+       return kind;
+    }
+}
