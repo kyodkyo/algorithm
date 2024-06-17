ad4f447d3acf21d73e272c732d1e265ec0bbfdcd
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-06-17 22:01:01 +0900

    [level 2] Title: [3차] n진수 게임, Time: 1.66 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; sb.length() < t*m; i++)
+            sb.append(Integer.toString(i, n));
+        
+        StringBuilder answer = new StringBuilder();
+        for(int i=p-1; answer.length() < t; i+=m)
+            answer.append(sb.charAt(i));
+        return answer.toString().toUpperCase();
+    }
+}
