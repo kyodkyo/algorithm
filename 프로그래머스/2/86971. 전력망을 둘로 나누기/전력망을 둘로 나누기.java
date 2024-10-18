f1f72be5408b8c27de4bf4e61e443d6562c37378
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-10-18 21:13:26 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.16 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,41 +1,49 @@
 import java.util.*;
 
-class Solution{
-    static int min = 100;
-    static ArrayList<ArrayList<Integer>> list;
-
+class Solution {
+    static ArrayList<Integer>[] list;
+    
     public int solution(int n, int[][] wires) {
-        list = new ArrayList<>();
+        list = new ArrayList[n+1];
         for(int i=0; i<=n; i++)
-            list.add(new ArrayList<>());
-
+            list[i] = new ArrayList<>();
+        
         for(int i=0; i<wires.length; i++){
-            list.get(wires[i][0]).add(wires[i][1]);
-            list.get(wires[i][1]).add(wires[i][0]);
+            int v1 = wires[i][0];
+            int v2 = wires[i][1];
+            list[v1].add(v2);
+            list[v2].add(v1);
         }
-
+        
+        int min = Integer.MAX_VALUE;
         for(int i=0; i<wires.length; i++){
-            list.get(wires[i][0]).remove(Integer.valueOf(wires[i][1]));
-            list.get(wires[i][1]).remove(Integer.valueOf(wires[i][0]));
-
+            int v1 = wires[i][0];
+            int v2 = wires[i][1];
+            
             boolean[] visited = new boolean[n+1];
+            list[v1].remove(Integer.valueOf(v2));
+            list[v2].remove(Integer.valueOf(v1));
+            
             int count = dfs(1, visited);
-            min = Math.min(min, Math.abs(count - (n-count)));
-
-            list.get(wires[i][0]).add(wires[i][1]);
-            list.get(wires[i][1]).add(wires[i][0]);
+            int diff = Math.abs(count - (n - count));
+            min = Math.min(min, diff);
+            
+            list[v1].add(v2);
+            list[v2].add(v1);
         }
+        
         return min;
     }
-
-    public static int dfs(int n, boolean[] visited){
-        visited[n] = true;
-
+    
+    public static int dfs(int start, boolean[] visited){
         int count = 1;
-        for(int v : list.get(n)){
+        visited[start] = true;
+        
+        for(int v : list[start]){
             if (!visited[v])
                 count += dfs(v, visited);
         }
+        
         return count;
     }
 }
